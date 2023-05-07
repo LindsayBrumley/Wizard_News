4adc1bf4280dfcbbf05e4e4141597d763aa2ddc5
LindsayBrumley <lalainaphillips@gmail.com>
2023-05-07 11:21:00 -0400

    error messages

--- app.js
+++ app.js
@@ -1,9 +1,100 @@
 const express = require("express");
+const morgan = require("morgan");
+const postBank = require("./postBank");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(morgan("dev"));
 
-const PORT = 1337;
+const DATABASE_URL =
+  "postgres://lorenipsum_user:tAnBJwF3LoBB0Ue0vNIPioSSanqP70uu@dpg-ch9sk7pjvhtimrbdlq2g-a.ohio-postgres.render.com/lorenipsum";
+
+const path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  const posts = postBank.list();
+
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts
+        .map(
+          (post) => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            <a href="/posts/${post.id}">${post.title}</a>
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+        )
+        .join("")}
+    </div>
+  </body>
+</html>`;
+  res.send(html);
+});
+
+app.get("/posts/:id", (req, res, next) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+  if (!post.id) {
+    res.status(404);
+    const html = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Wizard News</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+      <header><img src="/logo.png"/>Wizard News</header>
+      <div class="not-found">
+        <p>404: Page Not Found</p>
+      </div>
+    </body>
+    </html>`;
+    res.send(html);
+  } else {
+    res.send(
+      `<!DOCTYPE html>
+   <html>
+   <head>
+     <title>Wizard News</title>
+     <link rel="stylesheet" href="/style.css" />
+   </head>
+   <body>
+     <div class="news-list">
+       <header><img src="/logo.png"/>Wizard News</header>
+       
+         <div class='news-item'>
+           <p>
+             <span class="news-position">${post.id}. ▲</span>
+             ${post.title}
+             <small>(by ${post.name})</small>
+           </p>
+           <small class="news-info">
+             ${post.upvotes} upvotes | ${post.date}
+           </small>
+         </div>
+       
+     </div>
+   </body>
+ </html>`
+    );
+  }
+});
+
+const { PORT = 8080 } = process.env;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
